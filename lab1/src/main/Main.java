3cbbbb5d3fa3e11f3a3c02e60dab4265dafbc206
Nickywest21 <158302385+Nickywest21@users.noreply.github.com>
2026-02-04 12:38:39 -0600

    todo

--- lab1/src/main/Main.java
+++ lab1/src/main/Main.java
@@ -1,4 +1,4 @@
-
+package com.lab1compiladores;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
@@ -6,18 +6,66 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
 
-            
-            String ruta = TxtManager.seleccionarArchivoTxt();
+        while (continuar) {
 
-            if (ruta == null) {
-                System.out.println("Programa terminado.");
-                return;
+            try {
+
+                //Se selecciona el archivo que se desea analizar
+                String ruta = TxtManager.seleccionarArchivoTxt();
+
+                if (ruta == null) {
+                    System.out.println("No se selecciono ningun archivo");
+                } else {
+
+                    //Es lo que usa el ANTRL para poder leer los caracteres 
+                    CharStream input = CharStreams.fromFileName(ruta);
+
+                    //Es creada por ANTRL para hacer convertir en tokens
+                    MiGramaticaLexer lexer = new MiGramaticaLexer(input);
+
+                    //Guarda los tokens en el orden que el parser necesite
+                    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+                    //Genere y recorre token para obtener su tipo e imprimirlo
+                    tokens.fill();
+                    System.out.println("\n----Tokens----");
+                    for (Token token : tokens.getTokens()) {
+                        String nombreToken = MiGramaticaLexer.VOCABULARY
+                                .getSymbolicName(token.getType());
+
+                        System.out.println(
+                                "Token: " + nombreToken +
+                                " | Texto: " + token.getText()
+                        );
+                    }
+
+                    //Trabaja con los tokens que se generaron para el parser 
+                    MiGramaticaParser parser = new MiGramaticaParser(tokens);
+
+                    //Va analizando desde el inicio y verifica que cumpla la estructura 
+                    parser.prog();
+
+                    System.out.println("\nLa estructura si cumple\n");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error del analisis:");
+                e.printStackTrace();
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            //Se le pregunta al usuario si quiere analizar otro documento
+            System.out.println("¿Desea analizar otro documento? (si/no)");
+            String respuesta = scanner.nextLine();
+
+            if (!respuesta.equalsIgnoreCase("si")) {
+                continuar = false;
+            }
         }
+
+        System.out.println("Muchas gracias por analizar sus documentos con nosotros");
+        scanner.close();
     }
 }

72d205d4b7a7c697bad8fe9029e36b698083900f
Nicolle West <pnwest@correo.url.edu.gt>
2026-04-21 00:25:17 -0600

    ingreso de variables en el traductor

---
+++ proyecto1/src/main/java/com/compiladores/Traductor.java
@@ -0,0 +1,104 @@
+package com.compiladores;
+
+import com.compiladores.antlr.MiGramaticaBaseVisitor;
+import com.compiladores.antlr.MiGramaticaParser;
+
+public class Traductor extends MiGramaticaBaseVisitor<Void> {
+
+    // -------- PROGRAMA --------
+    @Override
+    public Void visitPrograma(MiGramaticaParser.ProgramaContext ctx) {
+
+        System.out.println("public class Programa {");
+
+        // funciones
+        for (var f : ctx.funciones()) {
+            visit(f);
+        }
+
+        // main
+        visit(ctx.main());
+
+        System.out.println("}");
+
+        return null;
+    }
+
+    @Override
+    public Void visitMain(MiGramaticaParser.MainContext ctx) {
+        System.out.println("public static void main(String[] args)");
+        visit(ctx.bloqueCodigo());
+        return null;
+    }
+
+    // -------- FUNCIONES --------
+    @Override
+    public Void visitFunciones(MiGramaticaParser.FuncionesContext ctx) {
+
+        String tipo = traducirTipo(ctx.tipo().getText());
+        String nombre = ctx.ID().getText();
+
+        System.out.print("public static " + tipo + " " + nombre + "(");
+
+        if (ctx.parametro() != null) {
+            String tipoParam = traducirTipo(ctx.parametro().tipo().getText());
+            String idParam = ctx.parametro().ID().getText();
+            System.out.print(tipoParam + " " + idParam);
+        }
+
+        System.out.println(")");
+
+        visit(ctx.bloqueCodigo());
+
+        return null;
+    }
+
+    // -------- VARIABLES --------
+    @Override
+    public Void visitVariables(MiGramaticaParser.VariablesContext ctx) {
+
+        String tipo = traducirTipo(ctx.tipo().getText());
+        String id = ctx.ID().getText();
+
+        if (ctx.expresiones() != null) {
+            System.out.println(tipo + " " + id + " = " + ctx.expresiones().getText() + ";");
+        } else {
+            System.out.println(tipo + " " + id + ";");
+        }
+
+        return null;
+    }
+
+    @Override
+    public Void visitAsignacion(MiGramaticaParser.AsignacionContext ctx) {
+        System.out.println(ctx.ID().getText() + " = " + ctx.expresiones().getText() + ";");
+        return null;
+    }
+
+    // -------- BLOQUES --------
+    @Override
+    public Void visitBloqueCodigo(MiGramaticaParser.BloqueCodigoContext ctx) {
+        System.out.println("{");
+
+        for (var s : ctx.sentencia()) {
+            visit(s);
+        }
+
+        System.out.println("}");
+        return null;
+    }
+
+    private String traducirTipo(String tipo) {
+        switch (tipo) {
+            case "intenso": return "int";
+            case "fenix": return "float";
+            case "dorado": return "double";
+            case "chispa": return "char";
+            case "bondad": return "boolean";
+            case "Cancion": return "String";
+            case "vasto": return "void";
+        }
+        return tipo;
+    }
+    
+}         

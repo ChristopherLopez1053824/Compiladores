e77781e33b34ad61a395de1d7015897504596787
Christopher Lopez <conradoclv@gmail.com>
2026-04-20 20:24:25 -0600

    Funcion de traduccion implementada falta complementar

--- proyecto1/src/main/java/com/compiladores/App.java
+++ proyecto1/src/main/java/com/compiladores/App.java
@@ -9,24 +9,21 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import com.compiladores.antlr.MiGramaticaLexer;
 import com.compiladores.antlr.MiGramaticaParser;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        boolean errores = false;
         Scanner scanner = new Scanner(System.in);
         boolean continuar = true;
 
-        while (continuar)
-        {
+        while (continuar) {
             try {
                 String ruta = TxtManager.seleccionarArchivoTxt();
-                if (ruta == null)
-                {
+                if (ruta == null) {
                     if (ruta == null) {
                         System.out.println("\n¿Desea analizar un documento? (si/no)");
                         String respuesta = scanner.nextLine();
@@ -38,54 +35,67 @@ public class App
                 }
 
                 CharStream input = CharStreams.fromFileName(ruta);
-                System.out.println("--------------------------------------------------------------------------------------------");
+                System.out.println(
+                        "--------------------------------------------------------------------------------------------");
                 MiGramaticaLexer lexer = new MiGramaticaLexer(input);
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
-               
+
                 tokens.fill();
                 for (Token t : tokens.getTokens()) {
-                        int tipo = t.getType();
+                    int tipo = t.getType();
 
-                        if (tipo == MiGramaticaLexer.UNCLOSED_COMMENT) {
-                            System.out.println("Error léxico en línea " + t.getLine() +": comentario sin cerrar");
-                        }
-
-                        else if (tipo == MiGramaticaLexer.INVALID_FLOAT) {
-                            System.out.println("Error léxico en línea " + t.getLine() +": número decimal mal formado -> " + t.getText());
-                        }
-
-                        else if (tipo == MiGramaticaLexer.INVALID_ID) {
-                            System.out.println("Error léxico en línea " + t.getLine() +": identificador inválido -> " + t.getText());
-                        }
-
-                        else if (tipo == MiGramaticaLexer.UNCLOSED_CHAR) {
-                            System.out.println("Error léxico en línea " + t.getLine() +": carácter sin cerrar -> " + t.getText());
-                        }
-
-                        else if (tipo == MiGramaticaLexer.UNCLOSED_STRING) {
-                            System.out.println("Error léxico en línea " + t.getLine() +": cadena sin cerrar -> " + t.getText());
-                        }
-
-                        else if (tipo == MiGramaticaLexer.ERROR_CHAR) {
-                            System.out.println("Error léxico en línea " + t.getLine() +": carácter inválido -> " + t.getText());
-                        }
-
-                        else{
-                            String nombreToken = MiGramaticaLexer.VOCABULARY.getSymbolicName(tipo);
-                            System.out.println("Token: " + nombreToken + " -> " + t.getText());
-                        }
+                    if (tipo == MiGramaticaLexer.UNCLOSED_COMMENT) {
+                        System.out.println("Error léxico en línea " + t.getLine() + ": comentario sin cerrar");
+                        errores = true;
                     }
-                System.out.println("--------------------------------------------------------------------------------------------");
+
+                    else if (tipo == MiGramaticaLexer.INVALID_FLOAT) {
+                        System.out.println("Error léxico en línea " + t.getLine() + ": número decimal mal formado -> "
+                                + t.getText());
+                        errores = true;
+                    }
+
+                    else if (tipo == MiGramaticaLexer.INVALID_ID) {
+                        System.out.println(
+                                "Error léxico en línea " + t.getLine() + ": identificador inválido -> " + t.getText());
+                        errores = true;
+                    }
+
+                    else if (tipo == MiGramaticaLexer.UNCLOSED_CHAR) {
+                        System.out.println(
+                                "Error léxico en línea " + t.getLine() + ": carácter sin cerrar -> " + t.getText());
+                        errores = true;
+                    }
+
+                    else if (tipo == MiGramaticaLexer.UNCLOSED_STRING) {
+                        System.out.println(
+                                "Error léxico en línea " + t.getLine() + ": cadena sin cerrar -> " + t.getText());
+                        errores = true;
+                    }
+
+                    else if (tipo == MiGramaticaLexer.ERROR_CHAR) {
+                        System.out.println(
+                                "Error léxico en línea " + t.getLine() + ": carácter inválido -> " + t.getText());
+                        errores = true;
+                    }
+
+                    else {
+                        String nombreToken = MiGramaticaLexer.VOCABULARY.getSymbolicName(tipo);
+                        System.out.println("Token: " + nombreToken + " -> " + t.getText());
+                    }
+                }
+                System.out.println(
+                        "--------------------------------------------------------------------------------------------");
                 tokens.seek(0);
                 MiGramaticaParser parser = new MiGramaticaParser(tokens);
                 parser.removeErrorListeners();
                 parser.addErrorListener(new BaseErrorListener() {
                     @Override
                     public void syntaxError(Recognizer<?, ?> recognizer,
-                                            Object offendingSymbol,
-                                            int line, int charPositionInLine,
-                                            String msg,
-                                            RecognitionException e) {
+                            Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg,
+                            RecognitionException e) {
 
                         String mensaje = msg;
                         mensaje = mensaje.replace("missing", "falta");
@@ -94,19 +104,31 @@ public class App
                         mensaje = mensaje.replace("mismenched input", "arreglo incorrecto");
                         mensaje = mensaje.replace("expecting", "se esperaba");
 
-                        System.out.println("Error en línea " + line +", columna " + charPositionInLine);
+                        System.out.println("Error en línea " + line + ", columna " + charPositionInLine);
                         System.out.println(mensaje);
                     }
                 });
-                parser.programa();
-        }
+                ParseTree tree = parser.programa();
+                if (errores == false) {
+                    System.out.println(
+                            "--------------------------------------------------------------------------------------------");
 
-        catch (Exception e) {
+                    System.out.println("\nVisitando nodos:");
+                    Traductor visitor = new Traductor();
+
+                    visitor.visit(tree);
+                }
+
+            }
+
+            catch (Exception e) {
                 System.out.println("Error durante el análisis:");
                 e.printStackTrace();
-        
-        System.out.println("Muchas gracias por usar el analizador.");
-        scanner.close();
-    }
 
-        }}}
+                System.out.println("Muchas gracias por usar el analizador.");
+                scanner.close();
+            }
+
+        }
+    }
+}
--- proyecto1/src/main/java/com/compiladores/Traductor.java
+++ proyecto1/src/main/java/com/compiladores/Traductor.java
@@ -1,5 +1,82 @@
 package com.compiladores;
 
-public class Traductor {
+import com.compiladores.antlr.MiGramaticaBaseVisitor;
+import com.compiladores.antlr.MiGramaticaParser;
+
+public class Traductor extends MiGramaticaBaseVisitor<Void> {
+
+    // -------- PROGRAMA --------
+    @Override
+    public Void visitPrograma(MiGramaticaParser.ProgramaContext ctx) {
+
+        System.out.println("public class Programa {");
+
+        // funciones
+        for (var f : ctx.funciones()) {
+            visit(f);
+        }
+
+        // main
+        visit(ctx.main());
+
+        System.out.println("}");
+
+        return null;
+    }
+
+    @Override
+    public Void visitMain(MiGramaticaParser.MainContext ctx) {
+        System.out.println("public static void main(String[] args)");
+        visit(ctx.bloqueCodigo());
+        return null;
+    }
+
+    // -------- FUNCIONES --------
+    @Override
+    public Void visitFunciones(MiGramaticaParser.FuncionesContext ctx) {
+
+        String tipo = traducirTipo(ctx.tipo().getText());
+        String nombre = ctx.ID().getText();
+
+        System.out.print("public static " + tipo + " " + nombre + "(");
+
+        if (ctx.parametro() != null) {
+            String tipoParam = traducirTipo(ctx.parametro().tipo().getText());
+            String idParam = ctx.parametro().ID().getText();
+            System.out.print(tipoParam + " " + idParam);
+        }
+
+        System.out.println(")");
+
+        visit(ctx.bloqueCodigo());
+
+        return null;
+    }
+
+    // -------- BLOQUES --------
+    @Override
+    public Void visitBloqueCodigo(MiGramaticaParser.BloqueCodigoContext ctx) {
+        System.out.println("{");
+
+        for (var s : ctx.sentencia()) {
+            visit(s);
+        }
+
+        System.out.println("}");
+        return null;
+    }
+
+    private String traducirTipo(String tipo) {
+        switch (tipo) {
+            case "intenso": return "int";
+            case "fenix": return "float";
+            case "dorado": return "double";
+            case "chispa": return "char";
+            case "bondad": return "boolean";
+            case "Cancion": return "String";
+            case "vasto": return "void";
+        }
+        return tipo;
+    }
     
-}
+}         
